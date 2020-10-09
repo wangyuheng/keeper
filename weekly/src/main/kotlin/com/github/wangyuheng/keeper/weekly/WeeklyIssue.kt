1c7f9e241ec6d227f9e5662cadc6b4cda6559925
wangyuheng <wangyuheng@outlook.com>
2020-10-09 20:33:53 +0800

    issue data

--- weekly/src/main/kotlin/com/github/wangyuheng/keeper/weekly/WeeklyIssue.kt
+++ weekly/src/main/kotlin/com/github/wangyuheng/keeper/weekly/WeeklyIssue.kt
@@ -1,3 +1,18 @@
 package com.github.wangyuheng.keeper.weekly
 
-class WeeklyIssue(var projectId: Int, var issueId: Int, var name: String, var description: String)
+import org.commonmark.ext.gfm.tables.TablesExtension
+import org.commonmark.parser.Parser
+import org.commonmark.renderer.html.HtmlRenderer
+
+private val extensions = listOf(TablesExtension.create())
+
+private val parser = Parser.builder()
+        .extensions(extensions)
+        .build()
+private val renderer = HtmlRenderer.builder()
+        .extensions(extensions)
+        .build()
+
+class WeeklyIssue(var projectId: Int, var issueId: Int, var name: String, var description: String) {
+    fun html(): String = renderer.render(parser.parse(this.description))
+}
